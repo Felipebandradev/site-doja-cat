ff9148bee52be8c86b875a56093f1e9fc928f6ce
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2023-10-02 13:23:47 -0300

    Estilização da index

--- index.php
+++ index.php
@@ -5,15 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doja Cat</title>
+    <link rel="stylesheet" href="css/estilos.css">
+
 </head>
 
 <body>
-    <main>
+    <header>
+    <div id="sidebar">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Albuns</a></li>
+            <li><a href="#">Inserir</a></li>
+            <li><a href="#">Editar</a></li>
+        </ul>
 
+    </div>
+
+    </header>
+    <main>
+        <section class="destaque-home">
+           
+            <h1>Boas-Vindas a <br>
+                Doja Cat</h1>
+
+                <p><a href="">EXPLORE</a>  <a onclick="toggleSidebar()"> Menu</a></p>
+        </section>
     </main>
     <footer>
         <p>site ficticio feito para fins educacionais</p>
     </footer>
+<script src="js/menu.js"></script>
 
 </body>
 
